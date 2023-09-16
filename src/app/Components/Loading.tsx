7dda47c599820fbfe67d95d3713f3b45c6b7941d
muhammad-1254 <usmansoomro1234@gmail.com>
2023-09-16 17:30:54 +0500

    remving unused imports

--- src/app/Components/Loading.tsx
+++ src/app/Components/Loading.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {AiOutlineCiCircle, AiOutlineClockCircle, AiOutlineCloudDownload, AiOutlineLoading, AiOutlineLoading3Quarters, AiOutlineReload, AiOutlineUpCircle} from 'react-icons/ai'
+import { AiOutlineCloudDownload, AiOutlineLoading3Quarters,} from 'react-icons/ai'
 const Loading = ({size}:any) => {
   return (
     <div className='w-screen h-screen z-50 
     flex items-center justify-center '>
         <div className='text-sky-400/95
-        relative animate-[spin_1.8s_linear_infinite]'>
+        relative animatec-[spin_1.8s_linear_infinite]'>
 
         <AiOutlineLoading3Quarters size={size}/>
 
